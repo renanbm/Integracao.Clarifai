0a05b72555eedf466dbbda4f563a326812795796
Renan Ben Moshe <renan.moshe@gmail.com>
2017-06-07 21:01:32 -0300

    Inclusão dos Unit Tests

---
+++ tests/clarifi_tests.py
@@ -0,0 +1,46 @@
+import unittest
+import Integracao
+
+
+class ClarifaiTests(unittest.TestCase):
+    def test_predict_by_url(self):
+        result = predict_by_url(r'https://samples.clarifai.com/metro-north.jpg', 'pt')
+        self.assertTrue(result == True)
+
+    def test_predict_demographics_by_url(self):
+        result = predict_demographics_by_url(r"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAmRAAAAJDMyMTM0NGE3LWJkZmMtNDNjZi04Y2U1LWQxYjdkYjFmZjJmYQ.jpg")
+        self.assertTrue(result == True)
+
+    def test_predict_demographics_by_path(self):
+        result = predict_demographics_by_path(r"C:\\Users\mr0028\Desktop\17894326.jpg")
+        self.assertTrue(result == True)
+
+
+def predict_by_url(url, language):
+    instancia = Integracao.ClarifaiPredictByUrl()
+    result = instancia.predict_by_url(url, language)
+    return checkJson(result)
+
+
+def predict_demographics_by_url(url):
+    instancia = Integracao.ClarifaiDemographics()
+    result = instancia.predict_demographics_by_url(url)
+    return checkJson(result)
+
+
+def predict_demographics_by_path(path):
+    instancia = Integracao.ClarifaiDemographics()
+    result = instancia.predict_demographics_by_path(path)
+    return checkJson(result)
+
+
+def checkJson(jsonContents):
+    test = True if "description" in jsonContents["status"] and jsonContents["status"]["description"] == "Ok" else False
+    return test
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
