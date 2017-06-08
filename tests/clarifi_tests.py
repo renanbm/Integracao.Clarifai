e4721dcea0f38b7d01940be34019b337134860cd
Renan Ben Moshe <renan.moshe@gmail.com>
2017-06-07 21:30:47 -0300

    Update clarifi_tests.py

--- tests/clarifi_tests.py
+++ tests/clarifi_tests.py
@@ -1,8 +1,14 @@
 import unittest
 import Integracao
+import helpers.Image_Helper as ImageHelper
 
 
 class ClarifaiTests(unittest.TestCase):
+    def test_predict_demographics_by_image(self):
+        imagebase64 = ImageHelper.get_path_as_pure_base64(r"C:\\Users\mr0028\Desktop\17894326.jpg")
+        result = predict_demographics_by_image(imagebase64)
+        self.assertTrue(result == True)
+        
     def test_predict_by_url(self):
         result = predict_by_url(r'https://samples.clarifai.com/metro-north.jpg', 'pt')
         self.assertTrue(result == True)
@@ -14,6 +20,12 @@ class ClarifaiTests(unittest.TestCase):
     def test_predict_demographics_by_path(self):
         result = predict_demographics_by_path(r"C:\\Users\mr0028\Desktop\17894326.jpg")
         self.assertTrue(result == True)
+
+
+def predict_demographics_by_image(imageBase64):
+    instancia = Integracao.ClarifaiDemographics()
+    result = instancia.predict_demographics_by_image(imageBase64)
+    return checkJson(result)
 
 
 def predict_by_url(url, language):
